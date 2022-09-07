1cf6db6cffff5ed49c6ac6d06cdbfdeddd37a476
CoolSulaiman <addvalue98@gmail.com>
2022-09-07 20:04:29 +0530

    Task 12

--- dom.js
+++ dom.js
@@ -7,17 +7,84 @@ const userList = document.querySelector('#users');
 
 myForm.addEventListener('submit', onSubmit);
 
+var bg=document.querySelector('.container');
+bg.style.background="#ccc";
+
+
 function onSubmit(e) {
+
  e.preventDefault();
-
-
-let myObj = {
+// local storage 
+ let myObj = {
 
     name : nameInput.value,
     email : emailInput.value
 };
 
+
 let myob_ser=JSON.stringify(myObj);
-localStorage.setItem("myobj",myob_ser);
- console.log(myob_ser);
+localStorage.setItem(email.value,myob_ser);
+
+
+
+
+Object.keys(localStorage).forEach((key) => {
+
+    stringifiedDetailsOfPeople = localStorage.getItem(key);
+       detailsOfPeople = JSON.parse(stringifiedDetailsOfPeople);
+       
+       const li = document.createElement('li');
+       console.log(detailsOfPeople);
+       li.append(detailsOfPeople.name +detailsOfPeople.email);
+       userList.append(li);
+   
+    
+   
+       const Editbtn=document.createElement('button');
+       Editbtn.appendChild(document.createTextNode("Edit"));
+        li.appendChild(Editbtn);
+   
+      const deletebtn=document.createElement('button');
+      deletebtn.appendChild(document.createTextNode("Delete"));
+      deletebtn.className='delete';
+       li.appendChild(deletebtn);
+   
+   
+       userList.addEventListener('click',removeitem);
+   
+       nameInput.value = '';
+       emailInput.value = '';
+       });
+
+       
+  function removeitem(e){
+
+    if(e.target.classList.contains('delete'))
+    {
+        if(confirm('are u sure'))
+        {
+            var d=e.target.parentElement;
+            userList.removeChild(d);
+            
+            localStorage.removeItem(email.value);
+
+
+        }
+    }
+
 }
+
+
+
+}
+
+
+
+
+
+    
+
+
+
+
+
