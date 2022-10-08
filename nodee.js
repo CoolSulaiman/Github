5464340ed1ff05106a32a9bb1b6659d508d1fcb2
CoolSulaiman <sulaimanchapti786@gmail.com>
2022-10-08 15:40:18 +0530

    url

--- nodee.js
+++ nodee.js
@@ -1,8 +1,39 @@
 const http =require('http');
 
-const server=http.createServer((req,rej)=>{
+const server=http.createServer((req,res)=>{
 
-console.log("Sulaiman")
+	const url =req.url;
+	if(url==='/home')
+	{
+	res.setHeader('Content-Type','text/html')
+	res.write('<html>')
+	res.write('<head><title>My first page</title></head>')
+	res.write('<body><h1> Welcome home </h1></body>')
+	res.write('</html>')
+	res.end();
+	}
+	if(url==='/about')
+{
+res.setHeader('Content-Type','text/html')
+res.write('<html>')
+res.write('<head><title>My first page</title></head>')
+res.write('<body><h1> Welcome to About Us page </h1></body>')
+res.write('</html>')
+res.end();
+}
+
+if(url==='/node')
+{
+res.setHeader('Content-Type','text/html')
+res.write('<html>')
+res.write('<head><title>My first page</title></head>')
+res.write('<body><h1> Welcome to my Node Js project </h1></body>')
+res.write('</html>')
+res.end();
+}
+console.log(req.url,req.method)
 })
 
 server.listen(4000)
+
+
