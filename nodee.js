5dd9875230e9173711bd670bb6bbad086cfb059c
CoolSulaiman <sulaimanchapti786@gmail.com>
2022-10-09 11:40:21 +0530

    messaage read from file and printed

--- nodee.js
+++ nodee.js
@@ -1,39 +1,54 @@
 const http =require('http');
-
+const fs= require('fs');
+const { buffer } = require('stream/consumers');
 const server=http.createServer((req,res)=>{
 
 	const url =req.url;
-	if(url==='/home')
+	const method=req.method;
+
+	fs.readFile("message.text",{encoding:"utf-8"}, (err,data)=>{
+		if(err){
+		console.log(err)
+		}
+	if(url==='/')
 	{
 	res.setHeader('Content-Type','text/html')
 	res.write('<html>')
 	res.write('<head><title>My first page</title></head>')
-	res.write('<body><h1> Welcome home </h1></body>')
+	res.write(`<body> ${data}</body}`)
+	res.write('<body><form action="/messaage" method="POST", id="IO"><input type="text" name="message"><button type="submit">Sent</button></form></body>')
 	res.write('</html>')
-	res.end();
 	}
-	if(url==='/about')
-{
-res.setHeader('Content-Type','text/html')
-res.write('<html>')
-res.write('<head><title>My first page</title></head>')
-res.write('<body><h1> Welcome to About Us page </h1></body>')
-res.write('</html>')
-res.end();
-}
-
-if(url==='/node')
-{
-res.setHeader('Content-Type','text/html')
-res.write('<html>')
-res.write('<head><title>My first page</title></head>')
-res.write('<body><h1> Welcome to my Node Js project </h1></body>')
-res.write('</html>')
-res.end();
-}
-console.log(req.url,req.method)
 })
 
+
+	if(url==="/messaage" && method==='POST'){
+		
+		const body=[];
+		req.on('data',((chunk)=>{
+				console.log(chunk)
+				body.push(chunk);
+		}));
+
+		req.on('end',(()=>{
+			const parsedBody=Buffer.concat(body).toString();
+			console.log(parsedBody)
+			const mes=parsedBody.split('=')[1]
+			fs.writeFileSync("message.text",mes);
+			res.write('')
+
+			
+
+		}));
+
+	res.statusCode=302;
+	res.setHeader('Location','/')
+	return res.end();
+	}
+	
+console.log(req.url,req.method)
+})
 server.listen(4000)
+
 
 
