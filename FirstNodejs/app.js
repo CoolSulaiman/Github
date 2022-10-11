75c2bae95bf18d0b2916d9cdf9145032418dfbf9
CoolSulaiman <sulaimanchapti786@gmail.com>
2022-10-11 17:50:25 +0530

    all abt express router

--- FirstNodejs/app.js
+++ FirstNodejs/app.js
@@ -1,31 +1,19 @@
-const http =require('http');
-const express= require('express')
+const express=require('express')
 const app=express();
-const bodyParser=require('body-parser')
 
-app.use(bodyParser.urlencoded({extended:false}))
+const adminRoutes=require('./route/admin')
+const shopRoute=require('./route/shop')
 
-app.use('/',(req,res,next)=>{
-    console.log("In the midlle  waare")
-    next()// ALLows the req to continue to next middlewaare in line
-})
-app.use('/addproduct',(req,res,next)=>{
-    console.log("Adding product");
-    res.send('<form action=/product method="POST"><label for="html">Product : </label><input type=text name="title"><label for="html">Size : </label> <input type=text name="size"><button type=submit>Add product</button></form>')
-    
+app.use('/admin',adminRoutes)
 
-})
+app.use('/shop',shopRoute)
 
-app.use('/product',(req,res,next)=>{
-    console.log(req.body);
-   res.redirect('/');
-})
-
- 
 app.use((req,res,next)=>{
-    console.log("In another midlle  waare")
-    res.send('<h1> Welcome express.js</h1>')
+
+    res.status(404).send('<h1> Page Not Found</h1>')
 })
+
+
 app.listen(4000);
 
 
